c3c817cd9ccd5e20b3416584193495c078a32b99
ottenby <ottenby123@gmail.com>
2025-11-11 18:37:34 +0100

    fetch pokemon info and translate into correct type

--- src/api/pokemon-api.ts
+++ src/api/pokemon-api.ts
@@ -4,10 +4,32 @@ import axios from "axios";
 const POKEAPI_BASE_URL = "https://pokeapi.co/api/v2";
 
 // Function to fetch data for a specific Pokemon by ID
-export const getPokemonById = async (id: number) => {
+export const getPokemonById = async (
+  id: number
+): Promise<PokemonDetailsType | null> => {
   try {
     const response = await axios.get(`${POKEAPI_BASE_URL}/pokemon/${id}`);
-    return response.data;
+
+    return {
+      name: response.data.name,
+      height: response.data.height,
+      weight: response.data.weight,
+      moves: response.data.moves.map((move: any) => move.move.name),
+      stats: response.data.stats.map((stat: any) => {
+        return {
+          baseStat: stat.base_stat,
+          effort: stat.effort,
+          stat: stat.stat.name,
+        };
+      }),
+      types: response.data.types.map((type: any) => {
+        return {
+          slot: type.slot,
+          type: type.type.name,
+        };
+      }),
+      image: response.data.sprites.other.dream_world.front_default,
+    };
   } catch (error) {
     console.error("Error fetching Pokemon data:", error);
     return null;
--- src/app/pokemon-list/[pokemonId]/page.tsx
+++ src/app/pokemon-list/[pokemonId]/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import { getPokemonById } from "@/api/pokemon-api";
 import { useQuery } from "@tanstack/react-query";
+import Image from "next/image";
 import { useParams } from "next/navigation";
 
 const PokemonDetailsPage = () => {
@@ -9,33 +10,44 @@ const PokemonDetailsPage = () => {
   if (!pokemonId) {
     return;
   }
-  console.log(pokemonId);
 
-  const { data } = useQuery({
-    queryKey: ["pokemonDetails"],
-    queryFn: getPokemonById(pokemonId),
+  const capitalizeFirstLetter = (val: string | undefined) => {
+    return String(val).charAt(0).toUpperCase() + String(val).slice(1);
+  };
+
+  const getPokemon = async () => {
+    return getPokemonById(pokemonId);
+  };
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["pokemon", pokemonId],
+    queryFn: getPokemon,
   });
-  //   const { data, error, isLoading, isError } = useQuery({
-  //     queryKey: ["pokemon"], //maybe should be id?
-  //     queryFn: () => {
-  //       getPokemonById(pokemonId);
-  //     },
-  //   });
-
-  //   if (isLoading) {
-  //     return <>...Loading</>;
-  //   }
-
-  //   if (isError) {
-  //     console.log(error as Error);
-  //     return <>...Error</>;
-  //   }
-
-  //   console.log("data:", data);
 
   return (
     <div>
-      <p>details page</p>
+      <h1>Name: {capitalizeFirstLetter(data?.name)}</h1>
+      {data?.image && (
+        <Image alt={data?.name} width={400} height={300} src={data?.image} />
+      )}
+      <p>Height: {data?.height}</p>
+      <p>Weight: {data?.weight}</p>
+      <div>
+        Moves:
+        {data?.moves.map((move, key) => (
+          <p key={key}>{move}</p>
+        ))}
+      </div>
+      <div>
+        Moves:
+        {data?.stats.map((stat, key) => (
+          <div key={key}>
+            <p>Base stat: {stat.baseStat}</p>
+            <p>Effort: {stat.effort}</p>
+            <p>Stat: {stat.stat}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };

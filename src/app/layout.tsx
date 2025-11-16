b6ef4b40aea1bf2c626ca04de935405ca35794da
ottenby <ottenby123@gmail.com>
2025-11-16 19:41:29 +0100

    commit
    remove unused imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Link from "next/link";
-import QueryProvider from "../providers/QueryProvider";
 import localFont from "next/font/local";
 import { Header } from "../components/Header/Header";
 
